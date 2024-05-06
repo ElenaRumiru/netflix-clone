d1a7a2a1c699f1d77514f9420926acea21c9a880
Elena Rumiru <elenarumiru@gmail.com>
2024-05-06 18:47:38 +0200

    Added movie`s collection with scrollbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../request'
 
 const Home = () => {
   return (
     <>
       <Main />
+      <Row rowID='1' title='Up Coming' fetchURL={requests.requestUpcoming} />
+      <Row rowID='2' title='Popular' fetchURL={requests.requestPopular} />
+      <Row rowID='3' title='Top Rated' fetchURL={requests.requestTopRated} />
+      <Row rowID='4' title='Trending' fetchURL={requests.requestTrending} />
+      <Row rowID='5' title='Horror' fetchURL={requests.requestHorror} />
     </>
   )
 }
--- src/request.js
+++ src/request.js
@@ -3,8 +3,8 @@ const key = 'f708b0c523d35f43f7b1af5ea9ae4387';
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page-1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page-1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page-2`,
-    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page-3`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=horror&include_adult=true&language=en-US&page=3`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page-1`,
 };
 
