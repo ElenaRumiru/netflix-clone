a3bfe9cf4d622eefdbcaef9c92a7481ac41b177e
Elena Rumiru <elenarumiru@gmail.com>
2024-05-07 15:24:12 +0200

    Changed:  backdrop_path to poster_path in Movie

--- src/request.js
+++ src/request.js
@@ -4,7 +4,7 @@ const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page-1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page-1`,
     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page-3`,
-    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=horror&include_adult=true&language=en-US&page=3`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=horror&include_adult=true&language=en-US&year=2023&page=1`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page-1`,
 };
 
