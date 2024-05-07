e105e6651760c36035e7d4262ee206447201fe06
Elena Rumiru <elenarumiru@gmail.com>
2024-05-07 17:52:58 +0200

    Added Firebase Authentification

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import Signup from './pages/Signup';
+import LogIn from './pages/LogIn';
+import Account from './pages/Account';
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
      <Navbar />
      <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={
+          <ProtectedRoute>
+            <Account />
+          </ProtectedRoute>}
+        />
      </Routes>
+    </AuthContextProvider>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+import userEvent from '@testing-library/user-event';
 
 const Navbar = () => {
+
+  const {user, logOut} = UserAuth();
+  const navigate =  useNavigate();
+  // console.log(user.email);
+
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
-        <div>
+      <Link to='/'>
+      <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+      </Link>        
+        {user?.email ? <div>
+          <Link to='/account'>
+            <button className='text-white pr-4'>Account</button>
+          </Link>
+            
+          <button onClick={handleLogOut} className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Log Out</button>
+                   
+        </div> : <div>
+          <Link to='/login'>
             <button className='text-white pr-4'>Sign In</button>
+          </Link>
+            
+          <Link to='/signup'>
             <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
-        </div>
+          </Link>            
+        </div>}
     </div>
   )
 }
