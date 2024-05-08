d818fdfd875a4212563646e4c1466bcd6ff7281b
Elena Rumiru <elenarumiru@gmail.com>
2024-05-08 17:26:19 +0200

    Added database and Saved Movies

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,8 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth"
+import { setDoc, doc } from 'firebase/firestore'
+
 
 const AuthContext = createContext();
 
@@ -8,7 +10,10 @@ export function AuthContextProvider({children}) {
     const [user, setUser] = useState({})
 
     function signUp (email, password) {
-        return createUserWithEmailAndPassword(auth, email, password);
+        createUserWithEmailAndPassword(auth, email, password);
+        setDoc(doc(db, 'users', email), {
+            savedShows: []
+        })
     };
 
     function logIn (email, password) {
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import SavedShows from '../components/SavedShows'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <>
+			<div className='w-full text-white'>
+				<img className='w-full h-[400px] object-cover' src='https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg' alt='' />
+				<div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+				<div className='absolute top-[20%] p-4 md:p-8'>
+					<h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>          
+				</div>
+			</div>
+      <SavedShows />
+    </>
   )
 }
 
